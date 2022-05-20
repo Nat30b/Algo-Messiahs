7611fbd5e470aa0a250ed42c5b27b025ec532d80
Nat30b <101274667+Nat30b@users.noreply.github.com>
2022-05-20 18:30:56 +0300

    Test_play
    A code named Test_Play_List.java for a music player by using linked list

---
+++ Test_Play_List.java
@@ -0,0 +1,48 @@
+//package player;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+import java.util.LinkedList;
+/**
+ *
+ * @author LENOVO
+ */
+public class Test_Play_List {
+	
+	static LinkedList<Song> playlist=new LinkedList<Song>();
+	static Player plr=new Player();
+    private static ArrayList<Play_List> play_lists = new ArrayList<Play_List>();
+    
+     public static void main(String[] args) {
+    
+        
+		main();
+        plr.play(playlist);
+    }
+   public static void main()
+		{
+			Play_List play_list = new Play_List("Ereft", "Yohana");
+		
+        play_list.addSong("Nabloda", 343);
+        play_list.addSong("Sakesh Yegodal", 334);
+        play_list.addSong("Edil", 347);
+        play_list.addSong("Berta", 449);
+        play_lists.add(play_list);
+        
+        play_list=new Play_List("Arcane League of Legends", "VA");
+        play_list.addSong("Playground", 352);
+        play_list.addSong("Enemy", 255);
+        play_list.addSong("Dynasties and Dynstopia", 300);
+        play_lists.add(play_list);
+        
+        
+        play_list.addSongToPlaylist("Playground", playlist);
+        play_list.addSongToPlaylist("Nabloda", playlist);
+        play_list.addSongToPlaylist("Berta", playlist);
+        play_list.addSongToPlaylist("Dynasty and Dynstopia", playlist);
+		}  
+}
